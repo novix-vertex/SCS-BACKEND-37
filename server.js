9504e02866d8126bacb090c547d475e4da93270c
chirag jain <jchirag0610@gmail.com>
2026-05-20 17:56:14 +0530

    note creation and display api

--- server.js
+++ server.js
@@ -3,9 +3,24 @@ const express = require('express')
 //creating server
 const app = express();
 
-app.get("/",(request,response)=>{
-    response.send("Hello!");
+//give express power to read client request body
+app.use(express.json());
+
+//created a blank notes array
+const notes = [];
+
+//api to create note
+app.post('/createNote', (req, res) => {
+    notes.push(req.body);
+    res.send("note created");
+});
+
+//api to read all the notes
+app.get('/getNotes', (req, res) => {
+    res.send(notes);
 });
 
 //start server that listen on the port no. 3000
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("server running on port no. 3000...");
+});
